c64fd1812e30c8a9b92411442895c466c55db898
shikailong <shikailonggy@163.com>
2018-11-11 23:01:12 +0800

    补全NIO聊天

--- src/main/java/com/gupao/nio/NioClient.java
+++ src/main/java/com/gupao/nio/NioClient.java
@@ -2,14 +2,19 @@ package com.gupao.nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
 
 public class NioClient {
 
-    private final InetSocketAddress serverAddress = new InetSocketAddress("localhost", 8088);
+    private final InetSocketAddress serverAddress = new InetSocketAddress("localhost", 8080);
     private Selector selector = null;
     private SocketChannel client = null;
 
@@ -31,17 +36,72 @@ public class NioClient {
         new Writer().start();
     }
 
-    class Reader extends Thread{
-        @Override
-        public void run() {
 
-        }
-    }
     class Writer extends Thread{
         @Override
         public void run() {
-
+            try{
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNext()){
+                    String line = scanner.nextLine();
+                    if("".equals(line)) continue;
+                    if("".equals(nickName)){
+                        nickName = line;
+                        line = nickName + USER_CONTENT_SPLIT;
+                    }else{
+                        line = nickName + USER_CONTENT_SPLIT + line;
+                    }
+                    client.write(charset.encode(line));
+                }
+                scanner.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
+
+    class Reader extends Thread{
+        @Override
+        public void run() {
+            try {
+                while (true){
+                    int readyChannels = selector.select();
+                    if(readyChannels == 0) continue;
+                    Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                    Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+                    while (keyIterator.hasNext()){
+                        SelectionKey key = keyIterator.next();
+                        keyIterator.remove();
+                        process(key);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void process(SelectionKey key) throws IOException {
+            if(key.isReadable()){
+                SocketChannel sc = (SocketChannel) key.channel();
+
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                String content = "";
+                while (sc.read(buffer) > 0){
+                    buffer.flip();
+                    content += charset.decode(buffer);
+                }
+                if(USER_EXIST.equals(content)){
+                    nickName = "";
+                }
+                System.out.println(content);
+                key.interestOps(SelectionKey.OP_READ);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new NioClient().session();
+    }
+
 
 }
--- src/main/java/com/gupao/nio/NioServer.java
+++ src/main/java/com/gupao/nio/NioServer.java
@@ -2,10 +2,9 @@ package com.gupao.nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.channels.SelectableChannel;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -45,16 +44,90 @@ public class NioServer {
             Iterator<SelectionKey> iterator = keys.iterator();
 
             while (iterator.hasNext()){
-                SelectionKey next = iterator.next();
+                SelectionKey key = iterator.next();
                 iterator.remove();
-                process(next);
+                process(key);
             }
         }
     }
 
-    private void process(SelectionKey next) {
-        
+    private void process(SelectionKey key) throws IOException {
+
+        // 判断客户端确定已经
+        if(key.isAcceptable()){
+            ServerSocketChannel server = (ServerSocketChannel) key.channel();
+            SocketChannel client = server.accept();
+
+            client.configureBlocking(false);
+            client.register(selector, SelectionKey.OP_READ);
+
+            key.interestOps(SelectionKey.OP_ACCEPT);
+            client.write(charset.encode("请输入你的昵称"));
+        }
+
+        if(key.isReadable()){
+
+            SocketChannel client = (SocketChannel) key.channel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            StringBuilder sb = new StringBuilder();
+            try {
+
+                while (client.read(buffer) > 0){
+                    buffer.flip();
+                    sb.append(charset.decode(buffer));
+                }
+
+                key.interestOps(SelectionKey.OP_READ);
+            } catch (IOException e) {
+                e.printStackTrace();
+                key.cancel();
+                if(key.channel() != null){
+                    key.channel().close();
+                }
+            }
+
+            if(sb.length() > 0){
+                String[] arrayContent = sb.toString().split(USER_CONTENT_SPLIT);
+                if(arrayContent != null && arrayContent.length == 1){
+                    String nickName = arrayContent[0];
+                    if(users.contains(nickName)){
+                        client.write(charset.encode(USER_EXIST));
+                    }else{
+                        users.add(nickName);
+                        int onlineCount = onLineCount();
+                        String message = "欢迎" + nickName + " 进入聊天室！当前在线人数：" + onlineCount ;
+                        briCast(null, message);
+                    }
+                }else if(arrayContent != null && arrayContent.length > 1){
+                    String nickName = arrayContent[0];
+                    String message = sb.substring(nickName.length() + USER_CONTENT_SPLIT.length()).toString();
+                    message = nickName + "说：" + message;
+                    if(users.contains(nickName)){
+                        briCast(null, message);
+                    }
+
+                }
+            }
+        }
+    }
+
+    private void briCast(Object o, String message) {
+
     }
 
 
+    private int onLineCount() {
+        int res = 0;
+        for(SelectionKey key : selector.keys()){
+            SelectableChannel target = key.channel();
+            if(target instanceof SocketChannel){
+                res ++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new NioServer(8080).listener();
+    }
 }
